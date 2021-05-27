c0bec188d211c35df6de9261e154a4a20c1b62e1
juventini1 <1326597589@qq.com>
2021-05-28 01:54:38 +0800

    '初始化项目'

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+@import "./assests/css/base.css";
+</style>
